5695219c99a22f3ecf95f319efdf4fe2a921574f
Gianmarco Montanari <gianmarco.montanari@iasonltd.com>
2025-05-25 18:19:31 +0200

    fix: try to fix github pages deploy pt.3

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter as Router } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
@@ -11,9 +11,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Router>
+        <HashRouter>
           <App />
-        </Router>
+        </HashRouter>
       </PersistGate>
     </Provider>
   </StrictMode>,
