55474402b037917734d9b9372fb7e28e60f6b6e3
Gianmarco Montanari <gianmarco.montanari@iasonltd.com>
2025-05-25 18:29:44 +0200

    fix: navbar position

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-dvh">
       <main className="flex-1 overflow-auto">
         <Routes>
           <Route path="/" element={<CardsList />} />
